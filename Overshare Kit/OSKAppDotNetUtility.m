3e6d4a5a876103bbedd18628af7dbc49c002cb9c
Jared Sinclair <jaredsinclair.rn@gmail.com>
2014-01-17 18:31:40 -0500

    Add curly braces around if statements.

--- Overshare Kit/OSKAppDotNetUtility.m
+++ Overshare Kit/OSKAppDotNetUtility.m
@@ -149,18 +149,22 @@ static NSString * OSKAppDotNetUtility_URL_WriteNewPost = @"stream/0/posts?access
                 
 				//There are times when the values aren't present in the dictionary
                 NSMutableDictionary *userInfo = [[NSMutableDictionary alloc] init];
-				if ([userDictionary objectForKey:kAccountID])
+				if ([userDictionary objectForKey:kAccountID]) {
 					userInfo[OSKAppDotNetUtility_UserInfoKey_accountID] = [userDictionary osk_nonNullStringIDForKey:kAccountID];
-                if ([userDictionary objectForKey:kUsername])
+                }
+                if ([userDictionary objectForKey:kUsername]) {
 					userInfo[OSKAppDotNetUtility_UserInfoKey_username] = [userDictionary osk_nonNullObjectForKey:kUsername];
-                if ([userDictionary objectForKey:kName])
+                }
+                if ([userDictionary objectForKey:kName]) {
 					userInfo[OSKAppDotNetUtility_UserInfoKey_name] = [userDictionary osk_nonNullObjectForKey:kName];
+                }
             
 				if ([userDictionary objectForKey:kAvatarObject])
 				{
 					NSDictionary *avatarObject = [userDictionary osk_nonNullObjectForKey:kAvatarObject];
-					if ([avatarObject objectForKey:kAvatarURL])
+					if ([avatarObject objectForKey:kAvatarURL]) {
 						userInfo[OSKAppDotNetUtility_UserInfoKey_avatarURL] = [avatarObject osk_nonNullObjectForKey:kAvatarURL];
+                    }
                 }
 				if (completion) {
                     dispatch_async(dispatch_get_main_queue(), ^{
