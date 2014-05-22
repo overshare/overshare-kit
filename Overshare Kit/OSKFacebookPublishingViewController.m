1bf3a94cd80d45e92f6090ed3c91db0d01397c18
Björn Roßborsky <bjoern.rossborsky@unister.de>
2014-05-22 15:16:21 +0200

    Use Localize Delegate for updateAudienceButton

--- Overshare Kit/OSKFacebookPublishingViewController.m
+++ Overshare Kit/OSKFacebookPublishingViewController.m
@@ -353,27 +353,28 @@
 
 #pragma mark - Audience Changes
 
-- (void)updateAudienceButton
-{
+- (void)updateAudienceButton {
 	NSString *audienceName = nil;
 	NSString *audienceKey = [self.activity currentAudience];
     
 	OSKPresentationManager *presManager = [OSKPresentationManager sharedInstance];
     
 	if ([audienceKey isEqualToString:ACFacebookAudienceEveryone]) {
-		audienceName = [presManager localizedText_FacebookAudience_Public];;
-	} else if ([audienceKey isEqualToString:ACFacebookAudienceFriends]) {
+		audienceName = [presManager localizedText_FacebookAudience_Public];
+	}
+    else if ([audienceKey isEqualToString:ACFacebookAudienceFriends]) {
 		audienceName = [presManager localizedText_FacebookAudience_Friends];
-	} else if ([audienceKey isEqualToString:ACFacebookAudienceOnlyMe]) {
+	}
+    else if ([audienceKey isEqualToString:ACFacebookAudienceOnlyMe]) {
 		audienceName = [presManager localizedText_FacebookAudience_OnlyMe];
 	}
+    
 	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
 		[self updateAudienceButton_Phone:audienceName];
 	} else {
 		[self updateAudienceButton_Pad:audienceName];
 	}
 }
-
 
 - (void)updateAudienceButton_Phone:(NSString *)audienceName {
     [self.audienceButton setTitle:[NSString stringWithFormat:@"%@", audienceName] forState:UIControlStateNormal];
