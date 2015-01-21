658e5d6b0ed3d3a1af6d9cdaaf663b24f087249d
Scott Hasbrouck <scott@ginkgotree.com>
2015-01-21 12:31:22 -0500

    Calls initSession in app delegate

--- Projects/iOS App/Overshare/Overshare/OSKAppDelegate.m
+++ Projects/iOS App/Overshare/Overshare/OSKAppDelegate.m
@@ -13,6 +13,7 @@
 #import "PocketAPI.h"
 
 #import "SampleTimelineViewController.h"
+#import "Branch.h"
 
 @implementation OSKAppDelegate
 
@@ -20,6 +21,41 @@
 {
 #warning  You must replace this key with your own app's key!
     [[PocketAPI sharedAPI] setConsumerKey:@"19568-eab36ebc89e751893a754475"];
+    
+#warning You must set your own Branch API key in Overshare-Info.plist as bnc_app_key (String). Find it here: https://dashboard.branch.io/#/settings
+    // Initialize Branch SDK
+    /*
+     http://branch.io
+     Branch enables custom downloads for every new user.
+     Drive higher conversions and more engagement using deep links that pass data through install and open.
+     
+     1. Track how many installs/opens come from each shared link.
+     2. Calculate viral kFactor
+     3. Insight into which share channels are driving the most downloads: Facebook, Twitter, etc.
+     4. Insight and analytics on which users are you rbiggest influencers and driving the most downloads.
+     5. Embed custom dictionaries that live on through clicking a link - even through the app store - to build a customized experience for each new user and make it really easy to deep link.
+     6. Customize links with OG tags to display content in Facebook, Twitter, etc.
+     7. Reward new or reffering users! Power the link with referral tracking, reward attribution, and credit balance!
+     */
+    
+    // A Pointer to the signleton instance of Branch. The first time this is called in the app lifecycle, a Branch instance is synchronously instatiated.
+    Branch *branch = [Branch getInstance];
+    
+#ifdef DEBUG
+    [Branch setDebug];
+#endif
+    
+    // Initiates a Branch session, and registers a callback. If you created a custom link with your own custom dictionary data, you probably want to know when the user session init finishes, so you can check that data. Think of this callback as your "deep link router". If your app opens with some data, you want to route the user depending on the data you passed in. Otherwise, send them to a generic install flow.
+    [branch initSessionWithLaunchOptions:launchOptions isReferrable:YES andRegisterDeepLinkHandler:^(NSDictionary *params, NSError *error) {
+        if (!error) {
+            NSLog(@"finished init with params = %@", [params description]);
+            
+            // example dictionary data
+            // NSString *name = [params objectForKey:@"user"];
+            // NSString *profileUrl = [params objectForKey:@"profile_pic"];
+            // NSString *description = [params objectForKey:@"description"];
+        }
+    }];
     
     UIWindow *window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     [self setWindow:window];
@@ -62,7 +98,9 @@
 
 #warning Don't forget to override this method so that Pocket, App.net and Google+ authentication have the opportunity to respond!
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    
     BOOL success = NO;
+    
     if ([[OSKADNLoginManager sharedInstance] application:application openURL:url sourceApplication:sourceApplication annotation:annotation]) {
         success = YES;
     }
@@ -71,6 +109,13 @@
     }
     else if ([GPPURLHandler handleURL:url sourceApplication:sourceApplication annotation:annotation]) {
         success = YES;
+    }
+    // Branch deep link handeler
+    // pass the url to the handle deep link call
+    // if handleDeepLink returns YES, and you registered a callback in initSessionAndRegisterDeepLinkHandler, the callback will be called with the data associated with the deep link
+    else if (![[Branch getInstance] handleDeepLink:url]) {
+        success = YES;
+        // do other deep link routing for the Facebook SDK, Pinterest SDK, etc
     }
     else {
         // if you handle your own custom url-schemes, do it here
